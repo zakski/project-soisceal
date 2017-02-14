d2f7b1cbd90f1799c0a68830f3ffb21e08d73f24
zakski <zak.szadowski+git@gmail.com>
2017-02-14 09:11:23 +0000

    refactoring of project structure to suit gradle

---
+++ 2p/src/test/java/TestURLClassLoaderClasses/acme/corp/MyException.java
@@ -0,0 +1,23 @@
+package TestURLClassLoaderClasses.acme.corp;
+
+@SuppressWarnings("serial")
+public class MyException extends Exception {
+	
+	String error = null;
+	public MyException()
+	{
+		super();            
+		error = "unknown";
+	}
+
+	public MyException(String err)
+	{
+		super(err);     
+		error = err; 
+	}
+
+	public String getError()
+	{
+		return error;
+	}
+}
