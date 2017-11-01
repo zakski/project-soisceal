c9561f1547f5016143e96cc488671d4280b153a7
zakski <zak.szadowski@gmail.com>
2017-11-01 16:24:17 +0000

    convert numeric prolog type classes to scala

---
+++ gospel-core/src/main/scala/com/szadowsz/gospel/core/data/Float.scala
@@ -0,0 +1,88 @@
+/*
+ * tuProlog - Copyright (C) 2001-2002  aliCE team at deis.unibo.it
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.szadowsz.gospel.core.data
+
+import java.util
+
+/**
+  * Float class represents the float prolog data type
+  */
+@SerialVersionUID(1L)
+class Float(val value: scala.Float) extends Number {
+
+  /**
+    * Returns the value of the Float as int
+    */
+  override final def intValue: scala.Int = value.toInt
+
+  /**
+    * Returns the value of the Float as float
+    */
+  override final def floatValue: scala.Float = value
+
+  /**
+    * Returns the value of the Float as double
+    */
+  override final def doubleValue: scala.Double = value
+
+  /**
+    * Returns the value of the Float as long
+    */
+  override final def longValue: scala.Long = value.toLong
+
+  /**
+    * is this term a prolog integer term?
+    */
+  override final def isInteger = false
+
+  /**
+    * is this term a prolog real term?
+    */
+  override final def isReal = true
+
+  /**
+    * Returns true if this Float term is grater that the term provided.
+    * For number term argument, the int value is considered.
+    */
+  override def isGreater(t: Term): Boolean = {
+    t.getTerm match {
+      case n : Number => value > n.floatValue
+      case term : Term => !term.isInstanceOf[Struct] && term.isInstanceOf[Var]
+    }
+  }
+
+  /**
+    * Tries to unify a term with the provided term argument.
+    * This service is to be used in demonstration context.
+    */
+  override def unify(vl1: util.List[Var], vl2: util.List[Var], t: Term, isOccursCheckEnabled: Boolean): Boolean = {
+    t.getTerm match {
+      case v: Var => v.unify(vl2, vl1, this, isOccursCheckEnabled)
+      case term: Term => term.isInstanceOf[Number] && term.asInstanceOf[Number].isReal && value == term.asInstanceOf[Number].floatValue
+    }
+  }
+
+  override def toString: String = java.lang.Float.toString(value)
+
+  /**
+    * @author Paolo Contessi
+    */
+  override def compareTo(o: Number): scala.Int = value.compareTo(o.floatValue)
+
+  override private[data] def unify(varsUnifiedArg1: util.List[Var], varsUnifiedArg2: util.List[Var], t: Term) = unify(varsUnifiedArg1, varsUnifiedArg2, t, true)
+}
---
+++ gospel-core/src/main/scala/com/szadowsz/gospel/core/data/Number.scala
@@ -0,0 +1,128 @@
+/*
+ * tuProlog - Copyright (C) 2001-2002  aliCE team at deis.unibo.it
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.szadowsz.gospel.core.data
+
+import java.util
+
+/**
+  *
+  * Number abstract class represents numbers prolog data type
+  *
+  * @see Int
+  * @see Long
+  * @see Float
+  * @see Double
+  *
+  *      Reviewed by Paolo Contessi: implements Comparable<Number>
+  */
+@SerialVersionUID(1L)
+abstract class Number extends Term with Comparable[Number] {
+
+  /**
+    * Returns the value of the number as int
+    */
+    def intValue: scala.Int
+
+  /**
+    * Returns the value of the number as float
+    */
+  def floatValue: scala.Float
+
+  /**
+    * Returns the value of the number as long
+    */
+  def longValue: scala.Long
+
+  /**
+    * Returns the value of the number as double
+    */
+  def doubleValue: scala.Double
+
+  /** is this term a prolog integer term? */
+  def isInteger: Boolean
+
+  /** is this term a prolog real term? */
+  def isReal: Boolean
+
+  /**
+    * Gets the actual term referred by this Term.
+    */
+  override def getTerm: Term = this
+
+  /** is this term a prolog numeric term? */
+  override final def isNumber = true
+
+  /** is this term a struct  */
+  override final def isStruct = false
+
+  /** is this term a variable  */
+  override final def isVar = false
+
+  override final def isEmptyList = false
+
+  /** is this term a constant prolog term? */
+  override final def isAtomic = true
+
+  /** is this term a prolog compound term? */
+  override final def isCompound = false
+
+  /** is this term a prolog (alphanumeric) atom? */
+  override final def isAtom = false
+
+  /** is this term a prolog list? */
+  override final def isList = false
+
+  /** is this term a ground term? */
+  override final def isGround = true
+
+  /**
+    * gets a copy of this term.
+    */
+  def copy(idExecCtx: Int): Term = this
+
+  /**
+    * gets a copy (with renamed variables) of the term.
+    * <p>
+    * the list argument passed contains the list of variables to be renamed
+    * (if empty list then no renaming)
+    */
+  override def copy(vMap: util.AbstractMap[Var, Var], idExecCtx: scala.Int): Term = this
+
+  /**
+    * gets a copy of the term.
+    */
+  override private[data] def copy(vMap: util.AbstractMap[Var, Var], substMap: util.AbstractMap[Term, Var]) = this
+
+  override def copyAndRetainFreeVar(vMap: util.AbstractMap[Var, Var], idExecCtx: scala.Int): Term = { // TODO Auto-generated method stub
+    this
+  }
+
+  override private[data] def resolveTerm(count: scala.Long) = count
+
+  /**
+    *
+    */
+  override def free(): Unit = {
+  }
+
+  private[data] def restoreVariables() = {
+  }
+
+  /*Castagna 06/2011*/
+  override def accept(tv: TermVisitor): Unit = tv.visit(this)
+}
